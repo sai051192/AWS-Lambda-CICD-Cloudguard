3752f8fd84148c451e8f346d6ee1eaeda0803d7b
sai051192 <61408815+sai051192@users.noreply.github.com>
2020-12-06 16:49:16 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+var time = require('time');
+exports.handler = (event, context, callback) => {
+    var currentTime = new time.Date(); 
+    currentTime.setTimezone("Asia/Kolkata");
+    callback(null, {
+        statusCode: '200',
+        body: 'The time in India is: ' + currentTime.toString(),
+    });
+};
